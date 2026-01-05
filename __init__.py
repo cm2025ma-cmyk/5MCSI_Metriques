5d7c250e09973af83958cc7e0c22bb6c8770cdf9
cm2025ma-cmyk <cholinmatthieu@gmail.com>
2026-01-05 17:07:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -23,9 +23,11 @@ def mongraphique():
 def histogramme():
     return render_template("histogramme.html")
 
-@app.route("/commits/")
-def commits():
-    return render_template("commits.html")
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
   
 @app.route('/tawarano/')
 def meteo():
