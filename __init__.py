e5b89f1ba7a38b8ec1422edc3e3080dfce0be981
cm2025ma-cmyk <cholinmatthieu@gmail.com>
2026-01-05 17:06:23 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,6 +22,10 @@ def mongraphique():
 @app.route("/histogramme/")
 def histogramme():
     return render_template("histogramme.html")
+
+@app.route("/commits/")
+def commits():
+    return render_template("commits.html")
   
 @app.route('/tawarano/')
 def meteo():
